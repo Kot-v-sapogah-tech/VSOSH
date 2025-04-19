57de0d4449bc2ccae87b82c57773e7de56f3ac3e
Kot-v-sapogah-tech <nagibimi@yandex.ru>
2025-04-19 10:51:42 +0300

    Update Kuznechik.py

--- Kuznechik.py
+++ Kuznechik.py
@@ -6,18 +6,15 @@ class KuznechikCipher:
         pass
 
     def _normalize_key(self, key: bytes) -> bytes:
-        """Дополняет ключ до 32 байт нулями, если он короче"""
         if len(key) > 32:
             raise ValueError("Ключ не должен превышать 32 байта.")
         return key.ljust(32, b'\x00')
 
     def _pad(self, data: bytes) -> bytes:
-        """PKCS#7 padding"""
         pad_len = 16 - (len(data) % 16)
         return data + bytes([pad_len] * pad_len)
 
     def _unpad(self, data: bytes) -> bytes:
-        """Удаление PKCS#7 padding"""
         pad_len = data[-1]
         if pad_len < 1 or pad_len > 16:
             raise ValueError("Неверная длина паддинга.")
